3beedc8927c3b82e77f640a30992c6ce9f31a9b8
alexkimiavi <alexsk98@gmail.com>
2020-12-23 21:42:59 -0500

    added some comments

--- backend/Endpoints/AddFriend.py
+++ backend/Endpoints/AddFriend.py
@@ -1,7 +1,9 @@
 from flask_restful import Resource
 
 
-class AddFriend(Resourse):
+"""Post Request"""
+class AddFriend(Resource):
     """ This will add a friend for the user """
+    """ The data will be a username of said friend"""
     def post(self):
         return {"Add":"Friend"}
--- backend/Endpoints/AddUser.py
+++ backend/Endpoints/AddUser.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 
 
+""" Post Request"""
 class AddUser(Resource):
     def post(self):
         return {"Add":"User"}
